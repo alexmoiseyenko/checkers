492c1d3dd4ead274ff8430f69b46c4ae9b32b49e
Alexey Moiseenko <beldeut@gmail.com>
2022-06-11 18:29:58 +0300

    refactor: burger menu

--- src/components/BurgerMenu/BurgerMenu.tsx
+++ src/components/BurgerMenu/BurgerMenu.tsx
@@ -15,18 +15,12 @@ const BurgerMenu: React.FC<BurgerMenuProps> = (props) => {
             onClick={() => setIsOpen(!isOpen)}
             className={styles.burgerMenu}
         >
-            <div className={clsx(
-                styles.burgerMenuLine,
-                {[ styles.burgerMenuFirstLineActive ]: isOpen}
-            )} />
-            <div className={clsx(
-                styles.burgerMenuLine,
-                {[ styles.burgerMenuSecondLineActive ]: isOpen}
-            )} />
-            <div className={clsx(
-                styles.burgerMenuLine,
-                {[ styles.burgerMenuThirdLineActive ]: isOpen}
-            )} />
+            <div
+                className={clsx(
+                    styles.burgerMenuLine,
+                    {[ styles.active ]: isOpen}
+                )}
+            />
         </div>
     );
 };
